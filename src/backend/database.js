1c340cba3497be6e96c3ef79ff011b11f4953e21
Yagmur Nielsen <127700012+yagmureva@users.noreply.github.com>
2024-03-14 12:49:01 +0100

    Update database.js

--- src/backend/database.js
+++ src/backend/database.js
@@ -11,6 +11,7 @@ const knex = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false },
   },
   pool: { min: 2, max: 10 },
 });

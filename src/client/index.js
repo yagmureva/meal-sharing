e874292be286bdcd94b3e2be80cca15129df825b
Yagmur Nielsen <127700012+yagmureva@users.noreply.github.com>
2024-03-14 14:04:57 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-<React.StrictMode>
- <App />
-</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
